8d6d43caf74ecf0c709206f7c64393aac97608c6
ANT <ant007h@gmail.com>
2010-07-04 22:18:19 +0400

    Read all QProcess output

--- src/raptor.cpp
+++ src/raptor.cpp
@@ -163,6 +163,7 @@ void RaptorMainWindow::runProc(QString cmd)
 {
     outbuf = "";
     aptProc = new QProcess(this);
+    aptProc->setProcessChannelMode(QProcess::MergedChannels);
     connect(aptProc, SIGNAL(readyRead()), this, SLOT(pReadyRead()));
     connect(aptProc, SIGNAL(finished(int, QProcess::ExitStatus)), this, SLOT(pFinished(int, QProcess::ExitStatus)));
     if ((mode == ModeDo) || (mode == ModeUpdate) || (mode == ModeConsole))
